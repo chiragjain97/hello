24777523319d7c44814b8f3a5da340dfd359960b
Chirag Jain <91095266+chiragjain97@users.noreply.github.com>
2021-09-21 05:00:27 +0530

    Pushed to Remote at

--- myscript.sh
+++ myscript.sh
@@ -4,12 +4,9 @@ git add .
 
 DATE=$(date)
 
-git -c core.whitespace=-trailing-space,-indent-with-non-tab,-tab-in-indent diff -U0 --word-diff-regex='[^[:space:]]'
-
-
 git diff main
 
-git commit -m "Changes made on $DATE"
+git commit -m "Pushed to Remote at $Date"
 
 git push 
 
