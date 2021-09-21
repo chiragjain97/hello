ce6a6bb3ea1baf9ff40899ee38b10083369626e6
Chirag Jain <91095266+chiragjain97@users.noreply.github.com>
2021-09-21 10:32:57 +0530

    Pushed to Remote at

--- myscript.sh
+++ myscript.sh
@@ -4,6 +4,8 @@ git add .
 
 git -c core.whitespace=-trailing-space,-indent-with-non-tab,-tab-in-indent diff -U0 --word-diff-regex='[^[:space:]]' main
 
+#123
+
 
 git commit -m "Pushed to Remote at"
 
