15764d9226ad68b8aa7342be1bfb5aa08f5f5882
Chirag Jain <91095266+chiragjain97@users.noreply.github.com>
2021-09-24 01:17:22 +0530

    Pushed to Remote at

--- myscript.sh
+++ myscript.sh
@@ -2,6 +2,8 @@
 
 #git -c core.whitespace=-trailing-space,-indent-with-non-tab,-tab-in-indent diff -U0 --word-diff-regex='[^[:space:]]' main
 
+ git diff main | grep -E 'android:id*|android:contentDescription.' 
+
 git add .
 
 git commit -m "Pushed to Remote at"
