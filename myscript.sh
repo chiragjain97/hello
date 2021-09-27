69b77176f826c290f07bc88cc31effea63d102d9
Chirag Jain <91095266+chiragjain97@users.noreply.github.com>
2021-09-27 18:13:47 +0530

    Pushed to Remote at

--- myscript.sh
+++ myscript.sh
@@ -4,9 +4,9 @@ git diff main > /Users/chirag.jain_rr/Desktop/test1.txt
 
 git diff main | grep -n -E "diff --git*|android:id*|android:contentDescription." > /Users/chirag.jain_rr/Desktop/test.txt
 
-number_of_lines=wc -l < /Users/chirag.jain_rr/Desktop/test.txt 
+number_of_lines=wc -l /Users/chirag.jain_rr/Desktop/test.txt 
 
-echo "Number of lines: $number_of_lines" >> /Users/chirag.jain_rr/Desktop/test1.txt
+echo "Number of lines: $number_of_lines" >> /Users/chirag.jain_rr/Desktop/test.txt
 
 git add .
 
