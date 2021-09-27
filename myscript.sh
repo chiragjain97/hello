4b4bab92d4cdd2042f93eb29bb8298b0422586d7
Chirag Jain <91095266+chiragjain97@users.noreply.github.com>
2021-09-28 00:31:56 +0530

    Pushed to Remote at

--- myscript.sh
+++ myscript.sh
@@ -2,11 +2,11 @@
 
 path=/Users/chirag.jain_rr/Desktop
 
+git diff main > $path/test1.txt
+
 git diff main | grep -n -E "diff --git*|android:id*|android:contentDescription." > $path/test.txt
 
 number_of_lines=`cat $path/test.txt | egrep -iv 'diff --git' | wc -l`
-
-changes=$((number_of_lines / 2))
 
 echo "Number of changes: $changes" >> $path/test.txt
 
